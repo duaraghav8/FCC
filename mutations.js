5e14b1def92d76ad7d021cecf57b265e87abb605
Raghav Dua <duaraghav8@gmail.com>
2016-05-20 20:06:23 +0530

    Delete mutations.js

--- mutations.js
+++
@@ -1,8 +0,0 @@
-/*
-  https://github.com/FreeCodeCamp/FreeCodeCamp/wiki/Algorithm-Mutations
-  Time complexity: O (N.log(N))
-*/
-
-function mutation (array) {
-  return array [0].toLowerCase ().split ('').sort ().join () === array [1].toLowerCase ().split ('').sort ().join ();
-}
